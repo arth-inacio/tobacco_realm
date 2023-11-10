bc225a1cddd7cb10b99f7f2a38eb7cbf74986292
Arthur <74980776+arth-inacio@users.noreply.github.com>
2023-11-09 23:56:10 -0300

    feat: Criando arquivos base da página de autenticação

---
+++ tobaccorealm/tobaccoauth/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TobaccoauthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tobaccoauth'
--- tobaccorealm/tobaccoproject/settings.py
+++ tobaccorealm/tobaccoproject/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tobaccoapp',
+    'tobaccoauth'
 ]
 
 MIDDLEWARE = [
